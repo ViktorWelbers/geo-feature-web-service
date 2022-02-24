ada9f9bef765eab0121c8c0ea8deeee1bcd67cb4
Viktor Welbers <v.welbers@gmx.de>
2022-02-24 01:48:36 +0100

    first commit

---
+++ index.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type Movie struct {
+	MovieID   string `json:"movieid"`
+	MovieName string `json:"moviename"`
+}
+
+type JsonResponse struct {
+	Type    string  `json:"type"`
+	Data    []Movie `json:"data"`
+	Message string  `json:"message"`
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/products", ProductsHandler)
+	r.HandleFunc("/articles", ArticlesHandler)
+	http.Handle("/", r)
+}
